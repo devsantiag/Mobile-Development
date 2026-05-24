58112f8ff7373cac56586c3aa6c703471888a13b
devsantiag <contatofasantiago@hotmail.com>
2026-05-23 23:42:36 -0300

    feat: add RegisterDesingComponent.kt

---
+++ mobileProjectPesquisaJuridica/KotlinProject/composeApp/src/commonMain/kotlin/org/example/project/components/userAccount/register/desingInterface/RegisterDesingComponent.kt
@@ -0,0 +1,9 @@
+package org.example.project.components.userAccount.register
+
+import androidx.compose.runtime.Composable
+
+// Responsible component for show user interface to register a new account in system
+@Composable
+fun UserInterfaceRegisterAccount () {
+
+}
