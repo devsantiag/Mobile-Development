f1051cbcf26ed6c546ddd34f0d662db8fcc6a68a
devsantiag <contatofasantiago@hotmail.com>
2026-05-25 11:39:53 -0300

    feat: add ButtonSubmitComponent.kt and HeaderInterfaceComponent.kt

---
+++ mobileProjectPesquisaJuridica/KotlinProject/composeApp/src/commonMain/kotlin/org/example/project/components/userAccount/register/button/ButtonSubmitComponent.kt
@@ -0,0 +1,8 @@
+package org.example.project.components.userAccount.register.button
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SubmitButton(){
+    
+}
---
+++ mobileProjectPesquisaJuridica/KotlinProject/composeApp/src/commonMain/kotlin/org/example/project/components/userAccount/register/desingInterface/HeaderInterfaceComponent.kt
@@ -0,0 +1,8 @@
+package org.example.project.components.userAccount.register.desingInterface
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Header() {
+
+}
