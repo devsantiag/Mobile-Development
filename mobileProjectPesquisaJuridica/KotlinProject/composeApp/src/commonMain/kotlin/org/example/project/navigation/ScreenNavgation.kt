57f1ddeb1299c499f5166db79235540bd0aad28b
devsantiag <contatofasantiago@hotmail.com>
2026-05-25 12:30:28 -0300

    Delete content

--- mobileProjectPesquisaJuridica/KotlinProject/composeApp/src/commonMain/kotlin/org/example/project/navigation/ScreenNavgation.kt
+++
@@ -1,8 +0,0 @@
-package org.example.project.navigation
-
-sealed class ScreenNavgation(val route: String) {
-    data object Login : ScreenNavgation("login")
-    data object Home : ScreenNavgation("home")
-    data object Register : ScreenNavgation("register")
-    data object ForgotPassword : ScreenNavgation("forgot_password")
-}
